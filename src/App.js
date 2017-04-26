82595088f3396b6162a4acaa963b476dd16adb60
Andy Nunez <anunez@mhelpdesk.com>
2017-04-26 08:38:29 -0400

    added two types of components stateless and state capable

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// State capable component
+class App extends React.Component {
+    render() {
+        return <h1>Hello my friend </h1>
+    }
+}
+
+// Stateless function version
+// const App = () => <h1>Hello stateless</h1>
+
+export default App
