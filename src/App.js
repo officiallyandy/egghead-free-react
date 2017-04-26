543a137f18cdf089322a7a4c690672b4cb9e2a1b
Andy Nunez <anunez@mhelpdesk.com>
2017-04-26 08:56:32 -0400

    Set props, prop types (data types) and required fields. Also defaulted prop values.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import React from 'react';
 // State capable component
 class App extends React.Component {
     render() {
+        const headerValue = this.props.txt;
+
         return (
             <section>
-                <h1>Wanna hear a joke?</h1>  
+                <h1>{headerValue}</h1>  
                 <p>A cat, a dog, and a fish walk into a bar ... </p>
             </section>
         )        
     }
+}
+
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: "Random text here"
 }
 
 // Stateless function version
