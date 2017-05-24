58ceab23f2554d17f8f865705b2f30dc2a6f45e3
Andy Nunez <anunez@mhelpdesk.com>
2017-05-24 13:17:34 -0400

    component mount / unmount button... I guess this mechanism can be used to create simple nav logic as well.

--- src/mountComponent.js
+++ src/mountComponent.js
@@ -39,9 +39,10 @@ class Wrapper extends React.Component {
             <div>
                 <button onClick={this.mount.bind(this)}> Mount </button>
                 <button onClick={this.unmount.bind(this)}> Unmount </button>
+                <div id="a"></div>
             </div>
         )
     }
 }
 
-export default App;
+export default Wrapper;
